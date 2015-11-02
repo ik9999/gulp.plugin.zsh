6a9898b9a896ad1e8dc1286d84dc48ed67d365c8
ik9999 <mailm743@yahoo.com>
2015-11-02 03:55:57 +0300

    error stream blocking

--- gulp.plugin.zsh
+++ gulp.plugin.zsh
@@ -1,16 +1,16 @@
 function $$gulp_completion () {
     if [ -f "gulpfile.js" ]; then
         files=("$(realpath gulpfile.js)")
-        requireDirVar="$(grep -Eo "[var|let].*require\(['\"]require\-dir['\"]\);$" gulpfile.js | grep -Eo " [^ =]*" | head -1| grep -Eo "[^ ]*")"
+        requireDirVar="$(grep -Eo "[var|let].*require\(['\"]require\-dir['\"]\);$" gulpfile.js 2>/dev/null | grep -Eo " [^ =]*" | head -1| grep -Eo "[^ ]*")"
         if [ ! -z "$requireDirVar" ]; then
-            requiredDirs="$(grep -Eo "requireDir\(['\"][^'\"]*" gulpfile.js | grep -Eo "['\"].*" | grep -Eo "['\"].*" | sed s/"['\"]"//g)"  
+            requiredDirs="$(grep -Eo "requireDir\(['\"][^'\"]*" gulpfile.js 2>/dev/null | grep -Eo "['\"].*" | grep -Eo "['\"].*" | sed s/"['\"]"//g)"  
             requiredDirsArray=("${(f)requiredDirs}")
             requiredFilesStr="$(find $requiredDirsArray -maxdepth 1 -name '*.js' -exec readlink -f {} \;)"
             requiredFilesArray=("${(f)requiredFilesStr}")
             files=($files $requiredFilesArray)
         fi
         filesUnique=(${(u)files[@]})
-        tasks="$(grep -Eo "gulp.task\(['\"]([^'\"]*)['\"]," $filesUnique | grep -Eo "['\"]([^'\"]*)['\"]" | sed s/"['\"]"//g | sort)"
+        tasks="$(grep -Eo "gulp.task\(['\"]([^'\"]*)['\"]," $filesUnique 2>/dev/null | grep -Eo "['\"]([^'\"]*)['\"]" | sed s/"['\"]"//g | sort)"
         completions=(${=tasks})
         compadd -- $completions
     fi
